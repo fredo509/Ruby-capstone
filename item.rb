19884dbdf0c022022de6eb098b164a466f5cdbcb
Daniel <carreraprogrammer@gmail.com>
2023-07-03 17:11:50 -0500

    Add spec for item and fix methods

--- item.rb
+++ item.rb
@@ -1,15 +1,29 @@
-require date 
+require 'date'
 
-Class Item
+class Item
+  attr_accessor :archived
 
-def initialize(genre, author, source, label)
-    @id = Random.rand(1...1000)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
-    @publish_date = date(day.month.year)
-    @archived = false
+  def initialize(genre, author, source, label, publish_date)
+      @id = Random.rand(1...1000)
+      @genre = genre
+      @author = author
+      @source = source
+      @label = label
+      @publish_date = Date.parse(publish_date)
+      @archived = false
+  end
+
+  def can_be_archived?
+    (Date.today - @publish_date).to_i > ( 10 * 365)
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+    else
+      @archived = false
+    end
+  end
 end
 
-
+book = Item.new('fantasy', 'j.k. rowling', 'amazon', 'scholastic', '1990-06-26')
