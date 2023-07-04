6988a154af5438b9a3a2d7b54b1497365860a921
Daniel <carreraprogrammer@gmail.com>
2023-07-04 11:43:33 -0500

    Fix linters

--- game.rb
+++ game.rb
@@ -2,10 +2,11 @@ require_relative 'item'
 require 'date'
 
 class Game < Item
-    attr_reader :multiplayer, :last_played_at
-    def initialize(genre, author, source, label, publish_date, multiplayer, last_played_at)
-      super(genre, author, source, label, publish_date)
-      @multiplayer = multiplayer
-      @last_played_at = Date.parse(last_played_at)
-    end
+  attr_reader :multiplayer, :last_played_at
+
+  def initialize(genre, author, source, label, publish_date, multiplayer, last_played_at)
+    super(genre, author, source, label, publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = Date.parse(last_played_at)
+  end
 end
