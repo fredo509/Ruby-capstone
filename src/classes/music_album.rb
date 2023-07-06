2e91502cd2d9b469dda3c0b75598883847048143
Fredo St Fleur <fredostfleur23@gmail.com>
2023-07-06 14:45:48 -0400

    Add name in initialize music

--- src/classes/music_album.rb
+++ src/classes/music_album.rb
@@ -1,11 +1,12 @@
 require_relative 'item'
 
 class Music < Item
-  attr_reader :on_spotify
+  attr_reader :on_spotify, :name
 
-  def initialize(publish_date, on_spotify)
+  def initialize(name, publish_date, on_spotify)
     super(publish_date)
     @on_spotify = on_spotify
+    @name =  name
   end
 
   def can_be_archived?
