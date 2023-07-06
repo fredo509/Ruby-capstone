7c6a1685a25500169c26df655b7a9ede2c819de3
Daniel <carreraprogrammer@gmail.com>
2023-07-05 20:12:39 -0500

    Add schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -16,3 +16,23 @@ CREATE TABLE items_labels (
   item_id INT REFERENCES items (id),
   label_id INT REFERENCES labels (id)
 )
+
+CREATE TABLE games(
+  id INT, 
+  name VARCHAR(255), 
+  published_date DATE, 
+  last_played_date DATE, 
+  multiplayer BOOLEAN, 
+  archived BOOLEAN, 
+  author_id INT REFERENCES authors(id), 
+  label_id INT REFERENCES labels(id), 
+  genre_id INT REFERENCES genres(id), 
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE authors(
+  id INT, 
+  first_name VARCHAR(255), 
+  last_name VARCHAR(255), 
+  PRIMARY KEY(id)
+);
