fa954893afa42e63c801335648369b709d9c4c46
Fredo St Fleur <fredostfleur23@gmail.com>
2023-07-06 17:52:22 -0400

    - Update render design - Add new music album to music json list

--- src/options/musicgenre_option.rb
+++ src/options/musicgenre_option.rb
@@ -8,26 +8,27 @@ class MusicGenreOptions
         @music_albums = []
         @genres = []
     end
-
     def add_music_album
-        puts 'Enter your music name'
-        name = gets.chomp
-        puts 'Is the music on spotify? (Y/N): '
-        on_spotify = gets.chomp.downcase == 'y'
-        puts 'Enter publish date (YYYY-MM-DD): '
-        publish_date = gets.chomp
+      puts 'Enter your music name'
+      name = gets.chomp
+      puts 'Is the music on Spotify? (Y/N): '
+      on_spotify = gets.chomp.downcase == 'y'
+      puts 'Enter publish date (YYYY-MM-DD): '
+      publish_date = gets.chomp
 
-        music = Music.new(name, publish_date, on_spotify)
-        add_genre(music)
-        @music_albums << music
+      music = Music.new(name, publish_date, on_spotify)
+      add_genre(music)
+
+      @music_albums << { name: music.name, publish_date: music.publish_date, on_spotify: music.on_spotify }
     end
+
     
     def add_genre(item)
         puts 'Enter genre name: '
         name = gets.chomp
         genre = Genre.new(name)
         genre.add_item(item)
-        @genres << genre
+        @genres << { name: genre.name}
         puts 'Genre added successfully!'
     end
  
@@ -39,7 +40,7 @@ class MusicGenreOptions
     puts "\n🔥 Listing music... 🖋️"
     sleep(0.5)
     puts ''
-    if @music_albums.empty?
+    if @music_albums.nil?
       puts "\n======================================================================"
       puts '||                                                                  ||'
       puts '||                        No music found 😿                       ||'
@@ -48,7 +49,7 @@ class MusicGenreOptions
       puts "I found #{@music_albums.length} Music:"
       puts ''
      all_music_album.each_with_index do |music_album, index|
-        puts "#{index + 1}. Music name: #{music_album.name}, Date published: #{music_album.publish_date}, Is on spotify ?: #{music_album.on_spotify}"
+        puts "#{index + 1}. Music name: #{music_album['name']}, Date published: #{music_album['publish_date']}, Is on spotify ?: #{music_album['on_spotify']}"
         end
       puts ''
     end
@@ -73,7 +74,7 @@ class MusicGenreOptions
       puts "I found #{@genres.length} Genre:"
       puts ''
       all_genres.each_with_index do |genre, index|
-        puts "#{index + 1}. #{genre.name}"
+        puts "#{index + 1}. #{genre['name']}"
         end
       puts ''
     end
