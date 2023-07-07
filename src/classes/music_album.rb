3fbe91eab78a11fb7553330c86be7124463d72ef
Fredo St Fleur <fredostfleur23@gmail.com>
2023-07-07 13:27:47 -0400

    Add id in music_album initializer

--- src/classes/music_album.rb
+++ src/classes/music_album.rb
@@ -1,10 +1,11 @@
 require_relative 'item'
 
 class Music < Item
-  attr_reader :on_spotify, :name
+  attr_reader :on_spotify, :name, :id
 
   def initialize(name, publish_date, on_spotify)
     super(publish_date)
+    @id = rand(1...100)
     @on_spotify = on_spotify
     @name =  name
   end
